18efda3ed69d6288fc268dfa6228a3de86f447b8
MateusSantosF <mateusferreira.dev@gmail.com>
2022-11-17 21:27:10 -0300

    Updated XML DOC i MailController

--- Controllers/MailController.cs
+++ Controllers/MailController.cs
@@ -6,16 +6,24 @@ using VoglioDire.Api.src.Models.Enums;
 namespace VoglioDire.Api2.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api")]
     public class MailController : ControllerBase
     {
 
         private readonly IEmailFacade _mailFacade;
+        /// <summary>
+        /// MailController constructor
+        /// </summary>
         public MailController(IEmailFacade emailFacade)
         {
             _mailFacade = emailFacade;
         }
 
+        /// <summary>
+        /// Send an email to the entered email address based on your type
+        /// </summary>
+        /// <param name="recipient">Destination e-mail address</param>
+        /// <param name="type">Type of mail</param>
         [HttpGet("send-mail")]
         public async Task<ActionResult> SendMail(string recipient, MailType type)
         {
